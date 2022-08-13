b87bbf2dfdb895982712d12cb0e3ab6a7ea5b42f
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-08-13 09:55:41 +0600

    update index js

--- js/index.js
+++ js/index.js
@@ -11,9 +11,9 @@ function loadDoc() {
 	}
 
 	xhttpRequest.open("GET", "http://localhost:5000/ajax_info.html");
-	console.log(xhttpRequest.getAllResponseHeaders());
-	//xhttpRequest.setRequestHeader('Access-Control-Allow-Origin:', '*');
-	httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	
+	xhttpRequest.setRequestHeader('Access-Control-Allow-Origin:', '*');
+	//httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
   	xhttpRequest.send();
 
