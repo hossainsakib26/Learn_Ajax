5e14bff8ae68930e2fc54c4e5cf2bb538e12ccfc
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-08-13 23:00:58 +0600

    now can access json data from reqres.in

--- js/index.js
+++ js/index.js
@@ -3,18 +3,33 @@ let msg = document.getElementById("heading");
 
 function loadDoc() {
 	// body...
-	const xhttpRequest = new XMLHttpRequest();
+	var xhttpRequest = new XMLHttpRequest();
 
-	xhttpRequest.onload = function () {
-		document.getElementById("getData").innerHTML = this.responseText;
-		console.log(xhttpRequest);
-	}
-
-	xhttpRequest.open("GET", "http://localhost:5000/ajax_info.html");
 	
-	xhttpRequest.setRequestHeader('Access-Control-Allow-Origin:', '*');
+	xhttpRequest.onreadystatechange  = getTestData;
+
+	xhttpRequest.open("GET", "https://reqres.in/api/users");
+	//xhttpRequest.open("POST", "ajax_info.txt");
+	//xhttpRequest.setRequestHeader('Access-Control-Allow-Origin:', '*');
 	//httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
+	xhttpRequest.responseType = 'json';
+
+	xhttpRequest.onload = () => {
+		const data = xhttpRequest.response;
+		console.log(data);
+	}
+
   	xhttpRequest.send();
+
+	function getTestData () {
+
+		if (this.readyState == 4 && this.status === 200) {
+			document.getElementById("getData").innerHTML = this.response;
+		}
+
+		console.log(xhttpRequest.readyState, xhttpRequest.status, this.getAllResponseHeaders());
+		//document.getElementById("getData").innerHTML = this.responseText;
+	}
 
 }
